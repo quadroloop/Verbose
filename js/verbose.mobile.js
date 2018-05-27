7a4cd51baf5a9bb5d7ffdd69573cbb96603af535
quadroloop <nplixel@gmail.com>
2018-05-27 09:37:37 -0700

    added menu ui functionality

--- js/verbose.mobile.js
+++ js/verbose.mobile.js
@@ -3,7 +3,8 @@
 
 function menu(object) {
 	var menubtn = object;
-	menubtn.className = "animated bounceReset";
+	menubtn.className = "animated bounceReset"
+	kernel(menubtn.id);
 	menubtn.onclick="clickReset()";
 }
 
@@ -11,4 +12,63 @@ function clickReset(object) {
 	var menubtn = object;
 	menubtn.className = "animated bounceIn";
 	menubtn.onclick= "menu()";
+}
+
+var vcount = 0;
+function kernel(item) {
+  switch(item) {
+  	case 'verbose' :
+  	    toggle_widgets();
+  	break;
+  	case 'preferences' :
+  	    toggle_widgets();
+  	break;
+  	case 'qr' :
+  	    toggle_widgets();
+  	break;
+  	case 'sbar' :
+  	    toggle_widgets();
+  	break;
+  }
+}
+
+function open1(){
+	if(vcount == 0){
+	    document.getElementById('sbar').style.display="block";
+    }else{
+        document.getElementById('sbar').classList.add('bounceOut');	
+    }
+}
+
+function open2(){
+	if(vcount == 0){
+	    document.getElementById('preferences').style.display="block";
+    }else{
+        document.getElementById('preferences').classList.add('bounceOut');	
+    }
+}
+
+function open3(){
+	if(vcount == 0){
+	    document.getElementById('qr').style.display="block";
+    }else{
+        document.getElementById('qr').classList.add('bounceOut');	
+    }
+}
+
+function toggle_widgets() {
+	 if(vcount == 0){
+           setTimeout('open1()',100);
+           setTimeout('open2()',200);
+           setTimeout('open3()',300);
+           vcount++;
+       }else{
+           setTimeout('open1()',100);
+           setTimeout('open2()',200);
+           setTimeout('open3()',300);
+           vcount=0;
+           document.getElementById('sbar').classList.remove('bounceOut');
+           document.getElementById('preferences').classList.remove('bounceOut');
+           document.getElementById('qr').classList.remove('bounceOut');
+       }
 }
