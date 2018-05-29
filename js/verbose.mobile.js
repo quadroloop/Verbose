7d0f99e5ce63b72896e37ced43f4a406b63baef5
quadroloop <nplixel@gmail.com>
2018-05-29 14:53:19 -0700

    automatically added changes via git-watch

--- js/verbose.mobile.js
+++ js/verbose.mobile.js
@@ -191,7 +191,9 @@ function backtosearch() {
   document.getElementById('panel').style.display = "none";
   document.getElementById('bg').style.display = "block";
   document.getElementById('search').value = '';
-  
+  document.getElementById('search').focus();
+   document.getElementById('search').click();
+
 
 }
 
